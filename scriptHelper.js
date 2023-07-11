aa1f5589a4b5dd73161694eb1cb407dc8f39a161
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-07-11 00:30:14 -0500

    check

--- scriptHelper.js
+++ scriptHelper.js
@@ -30,10 +30,11 @@ function validateInput(testInput) {
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     if(validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert("All fields are required!");
-    } else if(validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+    } else if(validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) !== "Not a Number" || validateInput(cargoLevel) !== "Not a Number") {
         alert("Enter correct input type!");
+    } else{
                 //alert("Everything is fine");
-        //list.style.visibility = "visible";
+        list.style.visibility = "visible";
         let pilotStatus = document.getElementById("pilotStatus");
             pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         let copilotStatus = document.getElementById("copilotStatus");
